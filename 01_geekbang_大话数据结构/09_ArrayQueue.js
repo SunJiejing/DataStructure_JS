290f2a8ef4945402f6ffb9aba121f2eebf86b9b6
SunJiejing <sunjiejing900911@163.com>
2021-07-19 23:55:32 +0800

    Update the enqueue() method.
    Added the data migration part when queue is full.

--- 01_geekbang_大话数据结构/09_ArrayQueue.js
+++ 01_geekbang_大话数据结构/09_ArrayQueue.js
@@ -14,16 +14,25 @@ class ArrayQueue{
         this.tail = 0;
 
     }
-
+// Update enqueue() by adding the element migration code snippets
     enqueue(item){
-        if(this.tail == this.queue.length) return false;
+        if(this.tail === this.queue.length){
+        if(this.head === 0){
+            return false;
+        }
+        for(let i = this.head; i < this.tail; i++){
+        this.queue[i - this.head] = this.queue[i];
+        }
+        this.tail -= this.head;
+        this.head = 0;
+        }
         this.queue[this.tail] = item;
         this.tail++;
         return true;
     }
 
     dequeue(){
-        if(this.head == this.queue.length) return null;
+        if(this.head === this.queue.length) return null;
         let item = this.queue[this.head];
         this.head++;
         return item;
