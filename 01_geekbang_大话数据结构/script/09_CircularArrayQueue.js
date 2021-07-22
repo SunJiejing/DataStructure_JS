609f651f30b542df66beb2f01b1953eac047947e
SunJiejing <sunjiejing900911@163.com>
2021-07-23 00:00:33 +0800

    Added Circular Array Queue js code.

---
+++ 01_geekbang_大话数据结构/script/09_CircularArrayQueue.js
@@ -0,0 +1,48 @@
+/*
+ * @Descripttion: Complish a circular queue base on an array
+ * @Author: JessieSun
+ * @Date: 2021-07-22 23:19:23
+ * @LastEditors: JessieSun
+ * @LastEditTime: 2021-07-22 23:57:33
+ */
+
+ class CircularQueue{
+     constructor(n){
+         this.queue = new Array(n);
+         this.size = n;
+         this.head = 0; //头下标
+         this.tail = 0; //尾下标
+     }
+
+     enqueue(data){
+         // 队列满时，无法再入队，故需判断队列满的情况
+         if((this.tail + 1)%this.size === this.head) return false;
+         this.queue[this.tail] = data;
+         this.tail = (this.tail + 1) % this.size; 
+         return true;
+     }
+
+     dequeue(){
+         // 队列空时，则无法出队
+         if(this.tail == this.head) return null;
+         var data = this.queue[this.head];
+         this.head = (this.head + 1) % this.size;
+         return data;
+     }
+ }
+
+ // test code
+ var queue = new CircularQueue(6);
+ console.log(queue.dequeue());  //  null;
+queue.enqueue("0");
+console.log(queue.tail);  // 1
+for(let i = 0; i < 6; i++){   // 这里对于最后一个元素，有bug
+    queue.enqueue("a" + i);
+}
+console.log(queue.head, queue.tail); // 0,0
+
+queue.dequeue();
+queue.dequeue();
+console.log(queue.head); //2
+queue.enqueue("7");
+console.log(queue.head, queue.tail) // 2,1
