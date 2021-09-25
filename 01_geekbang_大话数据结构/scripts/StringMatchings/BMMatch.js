77c6f8d9c910e58d63603ac2990bf01d934eaffe
JessieSun <jessie.sun@grapecity.com>
2021-09-25 14:54:38 +0800

    Update the BM method, still exists bug.

--- 01_geekbang_大话数据结构/scripts/StringMatchings/BMMatch.js
+++ 01_geekbang_大话数据结构/scripts/StringMatchings/BMMatch.js
@@ -3,17 +3,13 @@ function bm(mainStr, modelStr){
     let size2 = modelStr.length;
     // step1. Build the hashMap of modelStr info
     let hashMap = new Array(256);
-    hashMap.forEach(element => {
-        element = -1;
-    });
+   for(let index = 0; index < 256; index++){
+       hashMap[index] = -1;
+   }
     generateHashArray(modelStr, size2, hashMap);
     let suffix = new Array(size2);
     let prefix = new Array(size2);
-    // Initize suffix and prefix
-    for(let index = 0; index < size2; index++){
-        suffix[index] = -1;
-        prefix[index] = false;
-    }
+   
     generateGS(modelStr, size2, suffix, prefix);
 
     let i = 0;
@@ -26,19 +22,62 @@ function bm(mainStr, modelStr){
             return i;   // means model string matched in main string, and i is the start location in mainstr
         }
         // bad string part
-        let move1 = j - hashMap[mainStr[i]];
+        let move1 = j - hashMap[mainStr[i].charCodeAt()];
         // good suffix
+        let move2 = 0;
+        if(j < size2 - 1){    // means exist goog suffix
+            move2 = moveByGS(j, size2, suffix, prefix);
+        };
+        i = i + Math.max(move1, move2);
 
     }
+    return -1;
 }
 
 function generateHashArray(str, size, hashMap){
     for(let index = 0; index < size; index++){
-        let location = parseInt(str[index]);
+        let location = str[index].charCodeAt();
         hashMap[location] = index;
     }
 }
 
 function generateGS(str, strSize, suffix, prefix){
-    
+     // Initize suffix and prefix
+     for(let index = 0; index < strSize; index++){
+        suffix[index] = -1;
+        prefix[index] = false;
+    }
+    // Dealing the suffix and prefix
+    for(let strIndex = 0; strIndex < strSize - 1; strIndex++){   // Get the common substr part from substr 0~size-1 with str itself
+        let position = strIndex;
+        let length = 0;   // the common substr part's size
+        while(position >=0 && str[position] === str[strSize - 1 - length]){
+            position--;
+            length++;
+            suffix[length] = position + 1;
+        }
+        if(position === -1) prefix[length] = true;
+        
+    }
 }
+
+function moveByGS(badStrStart, modelStrSize, suffix, prefix) {
+    let goodSuffixLen = modelStrSize - 1 - badStrStart;    // the lenth of good suffix
+    if(suffix[goodSuffixLen] != -1 )  return badStrStart -  suffix[goodSuffixLen] + 1;   // means we found the matched substr of good suffix
+    // if didn't find the matched substr of good suffix
+    for(let index = badStrStart + 2; index <= modelStrSize - 1; index++ ){
+        if(prefix[modelStrSize - index] === true){     // means the modelStrSize - index length substr has the matched prefix, and need to move the same length to allign 
+            return index;
+        }
+    }
+    return modelStrSize;   // means there is no matched suffix and substr of suffix, so need to move the whole modelStr length  
+}
+
+
+
+
+
+// Testing code
+let mainstr = "IFinallyCatchedThePointOfBMMethodAndFeelSoHappy";
+let modelStr = "BM";
+console.log(bm(mainstr, modelStr));
