1201e6f85b98c8b9980d30acd3ac7641b272de05
SunJiejing <sunjiejing900911@163.com>
2022-10-04 21:00:41 +0800

    Update StackByLinkedList.js
    Adding the reverse linked list method by using stack property.

--- 03_DataStructure_IndiaBro/StackByLinkedList.js
+++ 03_DataStructure_IndiaBro/StackByLinkedList.js
@@ -27,6 +27,37 @@ Stack.prototype.isEmpty = function(){
     return this.top ? true:false;
 }
 
+var LinkedList = function(){
+    this.head = null;
+}
+LinkedList.prototype.insert = function(data){
+    var node = new Node(data);
+    if(!this.head){
+        this.head = node;
+        return;
+    }
+    node.next = this.head;
+    this.head = node;
+}
+LinkedList.prototype.reverse = function(){
+    if(!this.head) return null;
+    var stack = new Stack();
+    var temp = this.head;
+    while(temp){
+        stack.push(temp.value);
+        temp = temp.next;
+    }
+    temp = stack.top;
+    this.head = temp;
+    stack.pop();
+    while(!stack.isEmpty()){
+        temp = stack.pop();
+        
+    }
+    temp.next = null;
+}
+
+
 let st = new Stack();
 st.push(1);
 st.push(2);
@@ -36,3 +67,16 @@ console.log(st);
 st.pop();
 st.pop();
 console.log(st);
+st.push(5);
+console.log(st);
+st.push(10);
+st.push(15);
+console.log(st);
+
+var linkedList = new LinkedList();
+linkedList.insert(4);
+linkedList.insert(6);
+linkedList.insert(8);
+console.log(linkedList);
+linkedList.reverse();
+console.log(linkedList);
