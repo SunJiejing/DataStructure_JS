c9d7719bea567a21cb8fa0435bf026f3b2a6d655
SunJiejing <sunjiejing900911@163.com>
2022-11-07 00:33:23 +0800

    Update the Binary tree.
    Adding loop & recursive way for Binary Search tree to find out the min/max node.

--- 03_DataStructure_IndiaBro/BinarySearchTree.js
+++ 03_DataStructure_IndiaBro/BinarySearchTree.js
@@ -59,6 +59,38 @@ BSTree.prototype.search = function(root, data){
     }
 }
 
+BSTree.prototype.finMinByloop = function(root){
+    if (!root) return "Empty tree";
+    while(root.left){
+        root = root.left;
+    }
+    return root.value;
+}
+
+BSTree.prototype.finMinByRecursion = function(root){
+    if(!root) return "Empty tree";
+    if(root.left){
+        return this.finMinByRecursion(root.left);
+    }
+    return root.value;
+}
+
+BSTree.prototype.finMaxByloop = function(root){
+    if(!root) return "Empty tree";
+    while(root.right){
+        root = root.right;
+    }
+    return root.value;
+}
+
+BSTree.prototype.finMaxByRecursion = function(root){
+    if(!root) return "Empty tree";
+    if(root.right){
+        return this.finMaxByRecursion(root.right);
+    }
+    return root.value;
+}
+
 
 
 
@@ -85,4 +117,8 @@ bst.root = bst.insert( bst.root, 5);
 console.log(bst);
 console.log(bst.search(bst.root, 5));
 console.log(bst.search(bst.root, 10));
+console.log(bst.finMinByloop(bst.root));
+console.log(bst.finMinByRecursion(bst.root));
+console.log(bst.finMaxByloop(bst.root));
+console.log(bst.finMaxByRecursion(bst.root));
 
